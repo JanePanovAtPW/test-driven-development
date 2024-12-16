79b55b5ffb26dce79812631bf164a6d45a8ea028
janepanov <panovjane@gmail.com>
2024-12-16 16:07:28 +0100

    Write minimal code for the tests to pass

---
+++ tests/ArticleTest.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Article;
+use PHPUnit\Framework\TestCase;
+
+class ArticleTest extends TestCase{
+    protected $article;
+
+    public function setUp(): void
+    {
+        $this->article = new App\Article;
+    }
+
+    public function testTitleIsEmptyByDefault(){
+        $this->assertEmpty($this->article->title);
+    }
+
+    public function testSlugIsEmptyWithNoTitle(){
+        $this->assertSame($this->article->getSlug(), "");
+    }
+
+    public function testSlugHasSpacesReplacedByUnderscores(){
+        $this->article->title = "An example article";
+
+        $this->assertEquals("An_example_article", $this->article->getSlug());
+    }
+
+    public function testSlugHasWhitespaceReplacedBySingleUnderscore(){
+        $this->article->title = "An   example  \n  article";
+
+        $this->assertEquals("An_example_article", $this->article->getSlug());
+    }
+
+    public function testSlugDoesNotStartOrEndWithAnUnderscore(){
+        $this->article->title = " An example article ";
+
+        $this->assertEquals("An_example_article", $this->article->getSlug());
+    }
+
+    public function testSlugDoesNotHaveAnyNonWordCharacters(){
+        $this->article->title = " Read! This! Now! ";
+
+        $this->assertEquals("Read_This_Now", $this->article->getSlug());
+    }
+}
